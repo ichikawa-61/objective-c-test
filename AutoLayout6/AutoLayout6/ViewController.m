39ffa556e102c5e49b6aec19ef01d1282d7add76
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-04-20 10:38:14 +0900

    課題6 完了しました

---
+++ AutoLayout6/AutoLayout6/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AutoLayout6
+//
+//  Created by Manami Ichikawa on 4/20/17.
+//  Copyright © 2017 Manami Ichikawa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
