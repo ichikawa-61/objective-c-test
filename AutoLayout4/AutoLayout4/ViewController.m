6581ee5cb3421e6bd02ad3d4385d2aae0f3a9883
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-04-20 10:12:48 +0900

    課題4 終了しました

---
+++ AutoLayout4/AutoLayout4/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  AutoLayout4
+//
+//  Created by Manami Ichikawa on 4/20/17.
+//  Copyright © 2017 Manami Ichikawa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.imageView.image = [UIImage imageNamed:@"Image"];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
